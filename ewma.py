cc1237921e410027b99142ab16825fc7202dc382
dannemarre <martin.dannelind@gmail.com>
2020-04-15 14:00:11 +0200

    stash

--- ewma.py
+++ ewma.py
@@ -9,9 +9,10 @@ def ewma(df):
     data = df['Flow (l/s)']                        # Plocka ut kolumnen med originaldatan. Vet inte om man ska göra detta på originaldatan eller flytande dygn egentligen.
     dataEWMA = data.ewm(com=100).mean()            # Räkna ut EWMA. Com är typ en skalär vet inte riktig hur viktig den är. Det verkar kanske vara lite som man själv vill.
     df['EWMA'] = dataEWMA                          # Skapa en ny kolumn i DataFramen som är EWMA
-    ax = plt.gca()                                 # Axlarna för ploten
+#    ax = plt.gca()                                 # Axlarna för ploten
 
-    df.plot(y='Flow (l/s)', color="blue",ax=ax)    # Plotta originaldatan
-    df.plot(y='EWMA', color='pink', ax=ax)         # Plotta EWMA
+#    df.plot(y='Flow (l/s)', color="blue",ax=ax)    # Plotta originaldatan
+#    df.plot(y='EWMA', color='pink', ax=ax)         # Plotta EWMA
 
+    plt.scatter(df.index, data)
     plt.show()                                     # Visa plotten
