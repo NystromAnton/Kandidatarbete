673c09e2e0499d9631fdda64dd007370179fc29b
Anton Nyström <anton1nystrom@gmail.com>
2020-04-15 11:38:20 +0200

    En graphicshandler som kan plotta flera plots i samma figure

--- runner.py
+++ runner.py
@@ -4,6 +4,7 @@ import shewhart as s
 import cusum as c
 import ewma as e
 import datahandler as dh
+import graphicshandler as gh
 
 # Datasets:
 aikido = "radata_vatten_aikido.csv"
@@ -14,10 +15,12 @@ sumo = "radata_vatten_sumo.csv"
 
 # datahandler omvandlar datan till ett värde per dygn
 
-df = dh.datahandler(sumo)               # Ändra det här argumentet för att testa olika dataset
+df = dh.dateMean(karate)               # Ändra det här argumentet för att testa olika dataset
 
-#s.shewhart(df)                         # Kalla på shewhart funktionen. Den kommer plotta shewhart och lägga till det i DataFramen.
-#c.cusum(df)                            # Kalla på cusum funktionen. Den kommer plotta cusum och lägga till det i DataFramen.
-e.ewma(df)                              # Kalla på ewma funktionen. Den kommer plotta EWMA och lägga till det i DataFramen.
+s.shewhart(df)                         # Kalla på shewhart funktionen. Den kommer plotta shewhart och lägga till det i DataFramen.
+c.cusum(df)                            # Kalla på cusum funktionen. Den kommer plotta cusum och lägga till det i DataFramen.
+e.o_ewma(df)                              # Kalla på ewma funktionen. Den kommer plotta EWMA och lägga till det i DataFramen.
+
+gh.plotAll(df)
 
 print(df)
--- shewhart.py
+++ shewhart.py
@@ -8,33 +8,33 @@ import matplotlib.pyplot as plt
 #Out: Inget
 #Side effect: ritar upp plottar
 
-def shewhart(df):                    
-    avg = df['Flow (l/s)'].mean()  #Räknar ut medelvärdet på "Flow (l/s)" columnen                               
-    df['avg'] = avg                #Lägger till en ny column med avg värdet                   
+def shewhart(df):
+    avg = df['Flow (l/s)'].mean()  #Räknar ut medelvärdet på "Flow (l/s)" columnen
+    df['avg'] = avg                #Lägger till en ny column med avg värdet
     std = df['Flow (l/s)'].std()   #Räknar ut standardavvikelsen för "Flow (l/s)" columnen
     UCL = avg + (3*std)            #Räknar fram uper control limit
-    LCL = avg - (3*std)            #Räknar fram lower control limit                    
-    df2 = df.copy()                #Skapar en kopia av dataframen 
+    LCL = avg - (3*std)            #Räknar fram lower control limit
+    df2 = df.copy()                #Skapar en kopia av dataframen
     lock = True                    #Bool för att veta när gränserna är optimerade
     while(lock):
-        lock = False               #Sätter låset till false 
+        lock = False               #Sätter låset till false
         a = len(df2['Flow (l/s)']) #Tar ut längden på columnen "Flow (l/s)"
         indexNames = df2[(df2['Flow (l/s)'] > UCL)|(df2['Flow (l/s)'] < LCL) ].index #Hittar alla index på datapunkter som har ett värde större än UCL och mindre än LCL
         df2.drop(indexNames , inplace=True) #Droppar alla som är på de beräknade indexarna
-        avg = df['Flow (l/s)'].mean()       #Beräknar om medelvärdet på "Flow (l/s)" columnen                         
-        df2['avg'] = avg                    #Sätter nya medelvärdem                 
+        avg = df['Flow (l/s)'].mean()       #Beräknar om medelvärdet på "Flow (l/s)" columnen
+        df2['avg'] = avg                    #Sätter nya medelvärdem
         std = df2['Flow (l/s)'].std()       #Beräknar ny standardavvikelse
         UCL = avg + (3*std)                 #Beräknar om UCL
         LCL = avg - (3*std)                 #Beräknar om LCL
         if(a > len(df2['Flow (l/s)'])):     #Kollar om några datapunkter droppades detta varv
             lock = True                     #Låser lock för att köra ett varv till i loopen
-    ax = plt.gca()                          #Något för plottarna            
-    df['UCL'] = UCL                         #Skapar en column med UCL 
+    ax = plt.gca()                          #Något för plottarna
+    df['UCL'] = UCL                         #Skapar en column med UCL
     df['LCL'] = LCL                         #Skapar en column med LCL
-    df.plot(y='Flow (l/s)', color="blue",ax=ax)  #plottar flödesdatan från column "Flow (l/s)"                          
-    df.plot(y='avg', color='black', ax=ax)       #Plottar en medelvärdeslinje                           
-    df.plot(y='UCL', color='red', ax=ax)         #Plottar UCL                          
-    df.plot(y='LCL', color='red', ax=ax)         #Plottar LCL                          
+    df.plot(y='Flow (l/s)', color="blue",ax=ax)  #plottar flödesdatan från column "Flow (l/s)"
+    df.plot(y='avg', color='black', ax=ax)       #Plottar en medelvärdeslinje
+    df.plot(y='UCL', color='red', ax=ax)         #Plottar UCL
+    df.plot(y='LCL', color='red', ax=ax)         #Plottar LCL
 
-    plt.show()                                   # Visa plotten
-
+    #plt.show()                                   # Visa plotten
+    return
