b8e3d68cf1d1ed704500b65d0b236b50cb59d8e8
Filip Åslin <filipaslin@gmail.com>
2020-04-27 15:00:14 +0200

    ändringar i cusum. v-masken funkar bättre. Ingen warning.

--- cusum.py
+++ cusum.py
@@ -4,10 +4,46 @@ import matplotlib
 import matplotlib.pyplot as plt
 import math
 
+def get_cusum2(data):
+    normalized = (data['Flow (l/s)'] - data['Flow (l/s)'].mean()) / data['Flow (l/s)'].std()                              # Normalisera datan i kolumnen.
+    s_low = [0]
+    s_high = [0]
+    delta = 1                                                                   # Delta är antal sigma som krävs för att masken ska reagera.
+    k = delta/2                                                                 # Lutningen i v-maskens linjer.
+    a = 0.0027
+    B = 0.01
+    d = 2 * math.log((1-B)/a)
+    h = d*k
+    vmask = [0]
+    for i in range(1, len(normalized)):                                               # Loopa igenom kolumnen.
+        high = max(0, s_high[i-1] + normalized[i] - normalized.mean() - k)
+        low = max(0, s_low[i-1] + normalized.mean() - k - normalized[i])
+        s_high.append(high)
+        s_low.append(low)
+        if high > h or low > h:
+            vmask.append(max(high, low))
+        else:
+            vmask.append(0)
+
+    data['cusum'] = pd.DataFrame(s_high).set_index(data.index)
+    data['v-mask'] = pd.DataFrame(vmask).set_index(data.index)
+
 def get_dates(data):
     datapoints_out_of_control = data[data['v-mask'] != 0].index #Tar ut alla index /dagar den är out of control.
     return datapoints_out_of_control
 
+# Funktion som tar kolumnen 'Flow (l/s)' i en DataFrame och räknar ut en CUSUM för den kolumnen.
+# Precondition: DataFramen måste ha en kolumn med namnet 'Flow (l/s)'.
+# In: En DataFrame.
+# Out: None
+# Sideeffect: Skapar en ny kolum i den givna DataFramen som heter 'cusum'.
+def cusum(df):
+    get_cusum2(df)               # Skapa en ny kolumn i DataFramen som heter 'cusum'. set_index synkar indexeringen på den nya kolumnen och DataFramen så att de är samma.
+    dates = get_dates(df)
+    return dates
+
+
+################################GAMMALT#########################################
 # Funktion som med hjälp av en v-mask identifierar när en cusum-kurva är ur kontroll.
 # Precondition: DataFramen måste innehålla en kolumn som heter 'cusum'
 # In: data = en DataFrame.
@@ -41,14 +77,3 @@ def get_cusum(data):
     for i in range(1, len(data)):                                               # Loopa igenom kolumnen.
         s.append(s[i-1] + normalized[i] - normalized.mean())                    # Räkna ut CUSUM värdet för den datapunkten och lgg till i listan.
     return pd.DataFrame(s)                                                      # Returnera CUSUMen som en DataFrame
-
-# Funktion som tar kolumnen 'Flow (l/s)' i en DataFrame och räknar ut en CUSUM för den kolumnen.
-# Precondition: DataFramen måste ha en kolumn med namnet 'Flow (l/s)'.
-# In: En DataFrame.
-# Out: None
-# Sideeffect: Skapar en ny kolum i den givna DataFramen som heter 'cusum'.
-def cusum(df):
-    df['cusum'] = get_cusum(df['Flow (l/s)']).set_index(df.index)               # Skapa en ny kolumn i DataFramen som heter 'cusum'. set_index synkar indexeringen på den nya kolumnen och DataFramen så att de är samma.
-    v_mask(df)                                                                  # Applicera v-masken på dataframen. En ny kolumn som heter 'v-mask' skapas i df.
-    dates = get_dates(df)
-    return dates
